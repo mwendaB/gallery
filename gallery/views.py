aab168d56bb94924f0ce964247df2f389ff3a032
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-26 07:51:37 +0300

    add the migrtions

--- gallery/views.py
+++ gallery/views.py
@@ -2,9 +2,9 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return HttpResponse('Welcome to the Gallery')
 from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return HttpResponse('Welcome to the gallery')
