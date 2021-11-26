ff78ecb2d9027f8b1441fdfaffd7396085c6b4c9
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-26 07:47:57 +0300

    add the views attributes

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
+from django.http  import HttpResponse
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
