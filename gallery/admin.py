ee51008cec9edf798cd90a11d48e03edf52ff5e5
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-27 18:36:03 +0300

    write the admin file structure

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Location,tags, Image
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Location)
+admin.site.register(tags)
