25f1ebfb96ee00dedeacb4b32ba85ba13096888f
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-27 22:56:47 +0300

    add the project test case files

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
 import datetime as dt
-from .models import Location, Image
+from .models import Location, Image,
 # Create your tests here.
 
-class 
+class LocationTestCase(TestCase):
+     def setUp(self):
+        Location.objects.create(name="Test Location")
+
+     def test_location_name(self):
+        location = Location.objects.get(name="Test Location")
+        self.assertEqual(location.name, "Test Location")
+
+     def test_location_str(self):
+        location = Location.objects.get(name="Test Location")
+        self.assertEqual(str(location), "Test Location")
