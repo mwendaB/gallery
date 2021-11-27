4e53eed7ff8037e7fd18f8ac92206326e1d84ed9
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-27 18:42:35 +0300

    add the location file

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 import datetime as dt
 
+class Location(models.Model):
+    name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 # Create your models here.
 class Image(models.Model):
