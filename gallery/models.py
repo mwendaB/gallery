72fe5a263bbb1861d05ae9367d20315e1422a21f
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-27 18:40:48 +0300

    add the image class

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 import datetime as dt
 
+
+
 # Create your models here.
 class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/', null = True)
+    location = models.ForeignKey(Location, null=True)
+    name = models.CharField(max_length=30)
+    time = models.DateTimeField(auto_now_add=True, null=True)
+    descripton = models.TextField()
