2586758b04f78977386e0aa4f72ecb542ea8749d
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-27 23:23:53 +0300

    add the files part

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('',views.index,name='galleryHome'),
+    url('search/',views.get_category,name='category'),
+    url('location/<str:search_location>/',views.get_location,name='location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
