a76cb6eff4b2a1ebc7b1cd4caabd91a95cd3d1eb
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-28 09:40:55 +0300

    add the templates code

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Location, Category, Image
 # Register your models here.
-from .models import Location, Category, Image
-class ImageAdmin(admin.ModelAdmin):
-    filter_horizontal = ('Category',)
+
 
 admin.site.register(Image)
 admin.site.register(Location)
--- gallery/models.py
+++ gallery/models.py
@@ -6,14 +6,14 @@ import datetime as dt
 class Location(models.Model):
     name = models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.name
-
     def save_location(self):
         self.save()
 
     def delete_location(self):
         self.delete()
+    
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
@@ -32,7 +32,7 @@ class Category(models.Model):
         cls.objects.filter(id=id).update(name=name)
         
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'photos/', null = True)
+    image = models.ImageField(upload_to = 'images/', null = True)
     name = models.CharField(max_length=30)
     descripton = models.TextField()
     location_taken = models.ForeignKey("Location", on_delete= models.CASCADE , null=True)
