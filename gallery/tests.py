ece0ded4fbca2f4fda757e23662459548cd0c7f3
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-29 02:00:22 +0300

    create the first test class

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 # Create your tests here.
 
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='Masai Mara')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locat = Location.objects.all()
+        self.assertTrue(len(locat) > 0) 
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        category = Location.objects.all()
+        self.assertTrue(len(category) == 0)
