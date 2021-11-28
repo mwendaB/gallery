b3f3b73d3bdd85e0e4a8fb7c1306bf77c9d2df80
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-29 02:55:24 +0300

    add test classes

--- gallery/tests.py
+++ gallery/tests.py
@@ -38,3 +38,52 @@ class CategoryTestClass(TestCase):
         self.category.delete_category()
         category = Category.objects.all()
         self.assertTrue(len(category) <= 0)
+
+
+
+class ImageTestClass(TestCase):
+    
+    def setUp(self):
+
+        self.location = Location(name='Masai Mara')
+        self.location.save_location()
+
+        self.category = Category(name='Wildlife')
+        self.category.save_category()
+
+        self.mara= Image(id=1,name = 'Elephants', descripton ='Testing images class model',location_taken=self.location,category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mara,Image))
+    
+    def test_save_image(self):
+        self.mara.save_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)
+
+    def test_delete_image(self):
+        self.mara.delete_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image)== 0)
+
+    def test_update_image(self):
+        self.mara.save_image()
+        self.mara.update_image(self.mara.id, 'images/img.jpg')
+        new_img = Image.objects.filter(image='media/images/img1.jpg')
+        self.assertFalse(len(new_img) > 0)
+
+    def test_get_image_by_id(self):
+        self.mara.save_image()
+        img = self.mara.get_image_by_id(self.mara.id)
+        images = Image.objects.filter(id=self.mara.id)
+        self.assertTrue(img, images) 
+
+    def test_search_by_category(self):
+        category = 'Wildlife'
+        found_img = self.mara.search_by_category(category)
+        self.assertFalse(len(found_img) > 1)  
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
